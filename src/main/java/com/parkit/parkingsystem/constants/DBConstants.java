b16e1aff5100278d6fc9e8f97d12f9be68994937
Noemie Menu <menunoemie.dev@outlook.com>
2021-01-12 23:37:01 +0100

    STORY#2 : 5%-discount for recurring users

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -6,7 +6,7 @@ public class DBConstants {
     public static final String GET_PARKING_SPOT = "select * from parking where PARKING_NUMBER = ? limit 1";
     public static final String UPDATE_PARKING_SPOT = "update parking set available = ? where PARKING_NUMBER = ?";
 
-    public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME) values(?,?,?,?,?)";
+    public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME, RECURRENT) values(?,?,?,?,?,?)";
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=?, RECURRENT=? where ID=?";
     public static final String IS_RECURRENT = "SELECT COUNT(*) FROM ticket WHERE VEHICLE_REG_NUMBER=?";
 
