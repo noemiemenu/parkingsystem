af08af8cbccd95fbb9f41d1d439bb13cf85deefd
Noemie Menu <menunoemie.dev@outlook.com>
2021-01-22 19:43:29 +0100

    - fix price by rounding the price for one hour by one hour

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -23,13 +23,17 @@ public class FareCalculatorService {
         Date inHour = ticket.getInTime();
         Date outHour = ticket.getOutTime();
 
-        float duration = Duration.between(inHour.toInstant(), outHour.toInstant()).getSeconds(); // get seconds between inHour & outHour
+        double duration = Duration.between(inHour.toInstant(), outHour.toInstant()).getSeconds(); // get seconds between inHour & outHour
         duration = duration / 3600.0f; // divide by 3600.0f for 1 hour
 
         if (duration <= 0.5f) {
             ticket.setPrice(0);
             return;
         }
+
+
+        duration = Math.ceil(duration);
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
@@ -51,7 +55,11 @@ public class FareCalculatorService {
      */
     private void applyDiscountOnRecurringVisits(Ticket ticket) {
         if (ticket.isRecurrent()) {
-            ticket.setPrice(ticket.getPrice() * 0.95);
+            double price = ticket.getPrice() * 0.95;
+            price = price*10;
+            price = Math.round(price);
+            price = price /10;
+            ticket.setPrice(price);
         }
     }
 }
